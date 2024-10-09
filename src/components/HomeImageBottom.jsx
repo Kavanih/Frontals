9814a6fc7419b68c9a3da0d8ea3134cc12b0e961
Elfque <100863540+Elfque@users.noreply.github.com>
2024-10-09 17:42:56 +0100

    image path

--- src/components/HomeImageBottom.jsx
+++ src/components/HomeImageBottom.jsx
@@ -1,11 +1,11 @@
 import { gsap } from "gsap";
-import image1 from "../assets/1.jpg";
-import image2 from "../assets/2.jpg";
-import image3 from "../assets/3.jpg";
-import image4 from "../assets/4.jpg";
-import image5 from "../assets/5.jpg";
-import image6 from "../assets/6.jpg";
-import image7 from "../assets/7.jpg";
+import image1 from "../assets/1.JPG";
+import image2 from "../assets/2.JPG";
+import image3 from "../assets/3.JPG";
+import image4 from "../assets/4.JPG";
+import image5 from "../assets/5.JPG";
+import image6 from "../assets/6.JPG";
+import image7 from "../assets/7.JPG";
 import { useEffect, useRef } from "react";
 
 const HomeImageBottom = () => {
