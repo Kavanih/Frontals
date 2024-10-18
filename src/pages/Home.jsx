1a1a45bd93cb168b0125a45a15b5eb0ff5f6a3b8
Elfque <100863540+Elfque@users.noreply.github.com>
2024-10-18 07:22:10 +0100

    image check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,8 @@ const Home = () => {
     <div className="w-screen h-screen overflow-hidden">
       <Navbar />
 
-      <div className="flex items-center justify-center md:mt-44 mt-20">
+      <div className="flex items-center justify-center h-[70vh]">
+        {/* <div className="flex items-center justify-center md:mt-44 mt-28"> */}
         <img src={Logo} alt="" className="w-4/5" />
       </div>
 
